627d82bffcc4132ab06c4e483a4468a03a0d297a
zbarnz <zbarnz@yahoo.com>
2021-03-06 14:48:29 -0600

    Fixed a problem with form submission

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function goToMyGit() {
+
+    var pass = document.getElementById("password");
+    if(pass.value == "SocNet"){
+        window.open('https://dahvd.github.io/Group17/homepage');
+    }
+    else{
+        window.open('socnet.org');
+        window.open('socnet.org');
+        window.open('socnet.org');
+        window.open('socnet.org');
+        window.open('socnet.org');
+        window.open('socnet.org');
+        window.open('socnet.org');
+        window.open('socnet.org');
+
+        alert('Password incorrect, IP has been logged.');
+    }
+
+    return false;
+}
