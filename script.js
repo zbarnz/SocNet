f6aa7c33693b03062b110cbe7962322c9460702f
zbarnz <zbarnz@yahoo.com>
2021-05-05 00:35:05 -0600

    Fixed Problems

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
+async function ipGrab() {
+  const response = await fetch("https://api.ipify.org/?format=json")
+    .then((results) => results.json())
+    .then((data) => {
+      console.log(data);
+      alert("Password incorrect, IP has been logged: " + data.ip);
+    });
+}
 
 function goToMyGit() {
+  var pass = document.getElementById("password");
+  if (pass.value == "SocNet") {
+    window.open("https://dahvd.github.io/Group17/homepage");
+  } else {
+    ipGrab();
+    $("#pass")[0].reset();
+  }
 
-    var pass = document.getElementById("password");
-    if(pass.value == "SocNet"){
-        window.open('https://dahvd.github.io/Group17/homepage');
-    }
-    else{
-        window.open('socnet.org');
-        window.open('socnet.org');
-        window.open('socnet.org');
-        window.open('socnet.org');
-        window.open('socnet.org');
-        window.open('socnet.org');
-        window.open('socnet.org');
-        window.open('socnet.org');
-
-        alert('Password incorrect, IP has been logged.');
-    }
-
-    return false;
+  return false;
 }
